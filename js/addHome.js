7cab0dac084e339f5619ec8369ce36626e9ef2da
Filiczinka <filiczinka@gmail.com>
2023-05-18 01:55:25 +0300

    add select to html and formating addHome js code

--- js/addHome.js
+++ js/addHome.js
@@ -1,15 +1,25 @@
 const body = document.querySelector('body');
-const section = document.createElement('section');
-const divText = document.createElement('div');
-const h2 = document.createElement('h2');
+const optionRooms = document.getElementById('rooms');
+const selectBtn = document.getElementById('add-home__select__button');
+
 const divPhoto = document.createElement('div');
 const img = document.createElement('img');
+img.src = '../img/banner.png';
 
-h2.className = 'add__home-title';
-h2.textContent = 'Налаштування';
+const section = document.createElement('section');
+section.className = 'add-home container';
+
+const h2 = document.createElement('h2');
+h2.className = 'add-home__title';
+h2.textContent = 'НАЛАШТУВАННЯ';
+
+const divText = document.createElement('div');
+divText.className = 'add-home__form';
 
 
 divText.appendChild(h2);
+divText.appendChild(optionRooms);
+divText.appendChild(selectBtn);
 section.append(divText, divPhoto);
 body.insertBefore(section, body.firstChild);
 divPhoto.appendChild(img);
