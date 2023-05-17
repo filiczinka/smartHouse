188d9d04d264b04a121c6e4f821e24bb2e78a6c9
Filiczinka <filiczinka@gmail.com>
2023-05-17 22:47:54 +0300

    add double br for home html

--- btn.js
+++ btn.js
@@ -1,22 +1,20 @@
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function () {
-	const newColor = randomColorBg();
-	btn.style.backgroundColor = newColor;
-});
+btn.addEventListener('click', randomColorBg);
 
 function randomColorBg() {
 	const r = Math.floor(Math.random() * 256);
 	const g = Math.floor(Math.random() * 256);
 	const b = Math.floor(Math.random() * 256);
-	return `rgb(${r}, ${g}, ${b})`;
+	const newColor = this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+	return newColor;
 }
 
 btn.addEventListener('click', createNewTab);
 
-	function createNewTab() {
-		setTimeout(function () {
-			window.open('addHome.js', '_blank');
-		},
-			500);
-	}
+function createNewTab() {
+	setTimeout(function () {
+		window.open('addHome.js', '_blank');
+	},
+		500);
+}
