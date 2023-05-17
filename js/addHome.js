c0d6f8074fe10142e5901447febe11f553ba8d4b
Filiczinka <filiczinka@gmail.com>
2023-05-18 02:39:23 +0300

    fix

--- js/addHome.js
+++ js/addHome.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const optionRooms = document.getElementById('rooms');
 const selectBtn = document.getElementById('add-home__select__button');
+const selectedRooms = document.getElementById('selected-rooms');
 
 const divPhoto = document.createElement('div');
 const img = document.createElement('img');
@@ -16,10 +17,20 @@ h2.textContent = 'НАЛАШТУВАННЯ';
 const divText = document.createElement('div');
 divText.className = 'add-home__form';
 
+selectBtn.addEventListener('click', function () {
+	let newElement = document.createElement('li');
+	newElement.textContent = optionRooms.options[optionRooms.selectedIndex].textContent;
+	selectedRooms.appendChild(newElement);
+
+	newElement.style.fontFamily = "'Yanone Kaffeesatz', sans-serif";
+	newElement.style.fontWeight = '400';
+	newElement.style.listStyle = 'none';
+});
 
 divText.appendChild(h2);
 divText.appendChild(optionRooms);
 divText.appendChild(selectBtn);
+divText.appendChild(selectedRooms);
 section.append(divText, divPhoto);
 body.insertBefore(section, body.firstChild);
 divPhoto.appendChild(img);
