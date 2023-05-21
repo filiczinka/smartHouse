cfff189c3e4c37d8e9593808c6c973051f735fbb
Filiczinka <filiczinka@gmail.com>
2023-05-21 14:54:27 +0300

    fix

--- js/myHome.js
+++ js/myHome.js
@@ -3,7 +3,7 @@ const items = Array.from(document.querySelectorAll('.option__item'));
 
 const radiusX = 200; // Радіус по осі X
 const radiusY = 150; // Радіус по осі Y
-const centerX = 200; // Координата центру по осі X
+const centerX = 170; // Координата центру по осі X
 const centerY = 0; // Координата центру по осі Y
 const startAngle = -Math.PI / 2;
 const angleStep = (Math.PI * 2) / items.length;
