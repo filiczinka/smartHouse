a58e0b03aff3b5bbf1e8759b26142c8e4f0ef275
Filiczinka <filiczinka@gmail.com>
2023-05-23 17:59:25 +0300

    delete

---
+++ js/rooms.mjs
@@ -0,0 +1,9 @@
+const light = document.querySelector('.light');
+light.addEventListener('click', function () {
+	console.log('Параметри вашого освітленн:');
+});
+
+function info() {
+let divInfo = document.createElement('div');
+
+}
