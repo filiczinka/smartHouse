1f349c8c3869b1fe0864e6e7209ccefaf1077d4f
Filiczinka <filiczinka@gmail.com>
2023-05-27 17:14:45 +0300

    internetSystem

---
+++ js/wi-fi.js
@@ -0,0 +1,34 @@
+import { activeBtn, offBtn, activeImg, offImg } from "./buttons.js";
+
+class InternetSystem {
+	constructor(state) {
+		this.state = state;
+	}
+	turnOnInternet() {
+		this.state = 'On';
+		console.log('Wi-Fi ввімкнено');
+	}
+	turnOffInternet() {
+		this.state = 'Off';
+		console.log('Wi-Fi вимкнено');
+	}
+}
+
+const internet = new InternetSystem('Wi-Fi');
+const internetBtnOn = document.querySelector('.wi-fi__on-btn');
+const internetImg = document.querySelector('.wi-fi');
+const internetBtnOff = document.querySelector('.wi-fi__off-btn');
+
+internetBtnOn.addEventListener('click', function activeInternet() {
+	activeBtn(internetBtnOff, internetBtnOn);
+	activeImg(internetImg);
+	internet.turnOnInternet();
+	console.log(internet);
+});
+
+internetBtnOff.addEventListener('click', function offInternet() {
+	offBtn(internetBtnOn, internetBtnOff);
+	offImg(internetImg);
+	internet.turnOffInternet();
+	console.log(internet);
+});
