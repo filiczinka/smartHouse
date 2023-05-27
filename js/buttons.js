a172d9d5347891cd44c39005f70e71a132457f3c
Filiczinka <filiczinka@gmail.com>
2023-05-27 14:43:46 +0300

    climateSystem

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,6 @@
+import { ClimateSystem } from "./climateSystem.js";
+
+//кнопки активного та пасивного стану
 export function activeBtn(event1, event2) {
 	event1.classList.remove('off');
 	event2.classList.add('active');
@@ -18,14 +21,23 @@ export function offImg(event) {
 	event.classList.remove('active');
 }
 
+
+//кнопка ввімкн та вимкнення кондиціонеру
 export function toggleConditioner() {
 	const button = document.querySelector(".conditioner__btn");
-
+	const tempImg = document.querySelector('.temperature');
+	const climateSystem = new ClimateSystem("Climate");
 	if (button.classList.contains("active")) {
 		button.classList.remove("active");
 		button.classList.add("off");
+		button.innerHTML = 'Вимкнено';
+		offImg(tempImg);
+		climateSystem.turnOffConditioner();
 	} else {
 		button.classList.remove("off");
 		button.classList.add("active");
+		button.innerHTML = 'Ввімкнено';
+		activeImg(tempImg);
+		climateSystem.turnOnConditioner();
 	}
 }
--- js/climateSystem.js
+++ js/climateSystem.js
@@ -1,15 +1,38 @@
-import { toggleConditioner } from "./buttons";
+import { toggleConditioner } from "./buttons.js";
 
-class ClimateSystem {
-	constructor(temp) {
+export class ClimateSystem {
+	constructor(temp, airConditioner) {
 		this.temp = temp;
+		this.airConditioner = airConditioner;
 	}
 	setTemperature(temp) {
 		this.temperature = temp;
 		console.log(`Температуру встановлено на ${temp}°C.`);
 	}
+	turnOnConditioner() {
+		this.airConditioner = true;
+		console.log('Кондиціонер ввімкнено!');
+	}
+	turnOffConditioner() {
+		this.airConditioner = false;
+		console.log('Кондиціонер вимкнено!');
+	}
 }
 
 
-const button = document.querySelector(".conditioner__btn");
-button.addEventListener('click', toggleConditioner);
+const climateSystem = new ClimateSystem("Climate");
+const condBtn = document.querySelector('.conditioner__btn');
+condBtn.addEventListener('click', function activeClimate() {
+	toggleConditioner();
+});
+
+const tempBtn = document.querySelector('.temperature__btn');
+tempBtn.addEventListener('click', function tempState() {
+	const currentTemp = document.getElementById('temperature__input').value;
+	if (currentTemp === '' || currentTemp > 33 || currentTemp < 16) {
+		alert(' Вкажіть значення температури від 16 до 33!');
+	} else {
+		climateSystem.setTemperature(currentTemp);
+		tempBtn.innerHTML = 'Задано';
+	}
+});
