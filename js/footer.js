de89b6d9b1acd65179b5924fb8c8f80fa9117f3f
Filiczinka <filiczinka@gmail.com>
2023-05-28 23:56:18 +0300

    zone

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,13 @@
 function getCurrentTime() {
 	const apiKey = 'PQ78RM49TFQH';
-	const apiUrl = `https://api.timezonedb.com/v2.1/list-time-zone?key=PQ78RM49TFQH&format=xml`;
-
+	const apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=xml&by=zone&zone=Europe%2FKiev`;
 
 	fetch(apiUrl)
 		.then(response => response.text())
 		.then(data => {
 			const parser = new DOMParser();
 			const xmlDoc = parser.parseFromString(data, 'text/xml');
+
 			const timeZoneElement = xmlDoc.getElementsByTagName('zoneName')[0];
 			const currentTimeElement = xmlDoc.getElementsByTagName('formatted')[0];
 
@@ -15,8 +15,8 @@ function getCurrentTime() {
 				const timeZone = timeZoneElement.textContent;
 				const currentTime = currentTimeElement.textContent;
 
-				const currentTimeElement = document.getElementById("current-time");
-				currentTimeElement.textContent = `Time Zone: ${timeZone}, Current Time: ${currentTime}`;
+				const resultElement = document.getElementById("current-time");
+				resultElement.textContent = `Time Zone: ${timeZone}, Current Time: ${currentTime}`;
 			} else {
 				throw new Error('Invalid XML structure or missing elements.');
 			}
